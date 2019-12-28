1e5d8c39bbc8e2ca5941005ae27b492c40c79b49
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2019-12-28 15:17:09 -0700

    Add Bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,8 +10,15 @@ import css from "../css/app.css"
 // Import dependencies
 //
 import "phoenix_html"
+import "bootstrap"
 
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+import {Socket} from "phoenix"
+import LiveSocket from "phoenix_live_view"
+
+let liveSocket = new LiveSocket("/live", Socket)
+liveSocket.connect()
