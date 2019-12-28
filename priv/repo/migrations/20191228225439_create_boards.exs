1156aeffc3a35378ab5afead2709be0939c10578
alex0112 <alarsen0112@gmail.com>
2019-12-28 15:58:56 -0700

    Modified migration creating boards table

--- priv/repo/migrations/20191228225439_create_boards.exs
+++ priv/repo/migrations/20191228225439_create_boards.exs
@@ -5,13 +5,11 @@ defmodule Narratus.Repo.Migrations.CreateBoards do
     create table(:boards) do
       add :private, :boolean, default: false, null: false
       add :title, :string
-      add :root, references(:cards, on_delete: :nothing)
       add :owner, references(:users, on_delete: :nothing)
 
       timestamps()
     end
 
-    create index(:boards, [:root])
     create index(:boards, [:owner])
   end
 end
